f70ad3eec5ce9f90d1b2c37fcd5f3d9e89553a39
Alson Shareef <alson.shareef@gmail.com>
2018-12-17 15:55:54 +1100

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+let items = [
+    [3, 4],
+    [2, 5]
+];
+
+// Adds every number of 'items' array to a new one and logs the new one 
+let logNumbers = () => {
+    let output = []
+
+    for (let i = 0; i <= items.length - 1; i++){
+        output.push([])
+        for(let a = 0; a <= 1; a++){
+            output[i].push(items[i][a])
+        }
+    }
+        console.log('Item numbers:\n', output)
+}
+
+logNumbers()
+
+// ADDITION
+
+let add = () => {
+    let output = []
+    let additionItems = [
+        [2, 3],
+        [1, 6]
+    ]
+    
+    for (let i = 0; i <= items.length - 1; i++){
+        output.push([])
+        
+        for(let a = 0; a <= 1; a++){
+            output[i].push(items[i][a])
+            output[i][a] += additionItems[i][a]
+        }
+    }
+    console.log('After adding to items array, the new array is:\n', output)
+}
+
+add()
+
